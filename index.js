d7e2be355bd20eb18498f56324ba86092771e9fd
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-04 18:07:36 +0600

    add routing

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import { createServer } from "node:http";
+import fs from "fs";
 
 const port = 8000;
 
 const server = createServer();
 
-server.on("request", (req, res) => {
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.end("Привет, мир!");
+server.on("request", async (req, res) => {
+  let indexPage = await fs.promises.readFile("index.html", "utf-8");
+  let aboutPage = await fs.promises.readFile("about.html", "utf-8");
+
+  if (req.url == "/") {
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.writeHead(200, { "content-type": "utf8" });
+    res.write(indexPage);
+    res.end();
+  } else if (req.url == "/about") {
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.writeHead(200, { "content-type": "utf8" });
+    res.write(aboutPage);
+    res.end();
+  } else {
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.end("404");
+  }
 });
 
 server.listen(port);
