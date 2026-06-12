0ce74d1bc5952411c94a1cf5585f375fac0337e7
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-12 18:02:55 +0600

    ref routing

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ const port = 8000;
 const server = createServer();
 
 server.on("request", async (req, res) => {
-  let indexPage = await fs.promises.readFile("index.html", "utf-8");
-  let aboutPage = await fs.promises.readFile("about.html", "utf-8");
+  let indexPage = await fs.promises.readFile("views/index.html", "utf-8");
+  let questionPage = await fs.promises.readFile("views/question.html", "utf-8");
+  let resultPage = await fs.promises.readFile("views/result.html", "utf-8");
 
   if (req.url == "/") {
     res.setHeader("Content-Type", "text/plain; charset=utf-8");
     res.writeHead(200, { "content-type": "utf8" });
     res.write(indexPage);
     res.end();
-  } else if (req.url == "/about") {
+  } else if (req.url == "/question") {
     res.setHeader("Content-Type", "text/plain; charset=utf-8");
     res.writeHead(200, { "content-type": "utf8" });
-    res.write(aboutPage);
+    res.write(questionPage);
+    res.end();
+  } else if (req.url == "/result") {
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.writeHead(200, { "content-type": "utf8" });
+    res.write(resultPage);
     res.end();
   } else {
     res.setHeader("Content-Type", "text/plain; charset=utf-8");
