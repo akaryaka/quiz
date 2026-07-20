f715460e90c3a140faa2cd47aeb5880041c7d76d
Andrey <akariaka@yandex.ru>
2026-07-20 20:14:08 +0600

    dev server, working

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
-import express from "express";
+import express, { text } from "express";
+import cors from "cors";
 
 const port = 8000;
 const app = express();
 
-app.listen(port);
+app.use(cors());
+app.use(express.json());
 
-app.get('/', function(req, res) {
-  res.send('hello, world')
+const questions = [
+  { id: 1, text: 'Вопрос 1. Столица Норвегии',  options: ['Рейкьявик', 'Осло', 'Лондон']}
+]
+
+app.get('/api/questions', (req, res) => {
+  res.json(questions)
 })
+
+app.listen(port);
