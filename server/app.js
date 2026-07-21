e207de9c3f5a3b1bde23b8660ec4c64e41fb22d6
Andrey <akariaka@yandex.ru>
2026-07-21 19:30:59 +0600

    add data in questions, fixed api/submit(working)

--- server/app.js
+++ server/app.js
@@ -8,11 +8,22 @@ app.use(cors());
 app.use(express.json());
 
 const questions = [
-  { id: 1, text: 'Вопрос 1. Столица Норвегии',  options: ['Рейкьявик', 'Осло', 'Лондон']}
+  { id: 1, text: 'Вопрос 1. Столица Норвегии',  options: ['Рейкьявик', 'Осло', 'Лондон'], status: true},
+  { id: 2, text: 'Вопрос 2. Столица Бельгии',  options: ['Брюссель', 'Рейкъявик', 'Брюгге'], status: false },
+  { id: 3, text: 'Вопрос 3. Столица Германии',  options: ['Бостон', 'Мюнхен', 'Берлин'], status: false}
 ]
 
 app.get('/api/questions', (req, res) => {
   res.json(questions)
+})
+
+app.post('/api/submit', (req, res) => {
+  // console.log(req.body);
+   res.status(200).json({ 
+      success: true, 
+      message: 'Данные успешно сохранены!',
+      receivedData: req.body 
+  });
 })
 
 app.listen(port);
