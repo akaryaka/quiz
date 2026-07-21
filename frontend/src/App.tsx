a45b6fb5b969bd3b8b1dfbac3cb905323aa3ef15
Andrey <akariaka@yandex.ru>
2026-07-21 20:16:29 +0600

    submit form to server, view questions(comment) working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,35 +4,66 @@ import './App.css'
 
 function App() {
   const [data, newData] = useState([]);
+  const [question, setQuestion] = useState('Рейкьявик');
 
-  const handleSubmit = (event: SubmitEvent) => {
+  const handleSubmit = async (event: SubmitEvent) => {
     event.preventDefault();
-    console.log('hello');
+    const dataSubmit = {
+      question: question,
+    };
+    try {
+      const response = await fetch('http://localhost:8000/api/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataSubmit), 
+      })  
+      const result = await response.json();
+
+      if (response.ok) {
+        console.log(result);
+      }
+    } catch(error) {
+      console.log(error);
+    }
   }
+
+  const handleChange = (event: any) => {
+    setQuestion(event.target.value)
+  }
+
   // заменить на axios
   useEffect(() => {
     fetch('http://localhost:8000/api/questions')
       .then(res => res.json())
       .then(data => {
-        console.log(data);
-        
+    
         newData(data)})
       .catch(err => console.error(err));
   }, []);
-
+  
   
   return (
     <>
       <h1>Квиз</h1>
-      <form onSubmit={handleSubmit} action="/">
-        <label htmlFor="">Вопрос 1. Столица Норвегии</label>
-        <select name="" id="">
-          <option value="1">Рейкьявик</option>
-          <option value="1">Осло</option>
-          <option value="1">Лондон</option>
+      <form onSubmit={handleSubmit} action="/api/submit" method='post'>
+        {/* {data.map(question => (
+          <>
+            <h2 key={question.id}>{question.text}</h2>
+            <select name="" id="">
+              {question.options.map((option, key) => <option key={key}>{option}</option>)}
+            </select>
+          </>
+          )
+          )} */}
+        <h2>Вопрос 1. Столица Норвегии</h2>
+        <select name="questionsSelect" value={question} onChange={handleChange} id="questions-select">
+          <option value="val1">Рейкьявик</option>
+          <option value="val2">Осло</option>
+          <option value="val3">Лондон</option>
         </select>
         <input type="submit" value='ответить'/>
-        {/* {data} */}
       </form>
     </>
   )
