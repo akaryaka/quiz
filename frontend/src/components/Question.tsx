eee878289f6df041f95222f74dbfcebc33f8a2c7
Andrey <akariaka@yandex.ru>
2026-07-25 19:56:17 +0600

    add comment with code

--- frontend/src/components/Question.tsx
+++ frontend/src/components/Question.tsx
@@ -1,11 +1,25 @@
-function Question() {
-  const variable: boolean = true;
+import { useState } from "react";
 
-  return(
-    <>
-      { variable ? 'sdsd' : 'sa'}
-    </>
-  )
+function Question() {
+  // export const [data, newData] = useState([]);
+  // const [question, setQuestion] = useState('Рейкьявик');
+  // // const variable: boolean = true;
+  // const handleChange = (event: any, id) => {
+  //   setQuestion(event.target.value)
+  // }
+
+  // return(
+  //   <>
+  //     {data.map(question => (
+  //       <>
+  //         <h2 key={question.id}>{question.text}</h2>
+  //         <select name={question.id} onChange={handleChange} id="questions-select">
+  //           {question.options.map((option, key) => <option key={key} value={key}>{option}</option>)}
+  //         </select>
+  //       </>
+  //     ))}
+  //   </>
+  // )
 }
 
 export default Question
