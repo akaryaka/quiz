d3c48ad5eb006f2b5fff67aad2c4929e6817f17c
Andrey <akariaka@yandex.ru>
2026-07-25 19:41:36 +0600

    add styles

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,37 @@
+import styled from "styled-components"
+
 function Home() {
+  const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    border: 1px solid #000;
+    padding: 20px;
+    border-radius: 10px;
+  `
+
+  const Title = styled.h1`
+    margin-bottom: 25px;
+  `
+
+  const Text = styled.p`
+    margin-bottom: 25px;
+  `
+
+  const BtnNext = styled.button`
+    padding: 5px;
+    width: 30%;
+    cursor: pointer;
+    margin: 0 auto;
+  `
+
   return (
     <>
-      <h1>Квиз</h1>
-      <p>Привет! Давай проверим, насколько хорошо ты знаешь столицы</p>
+      <Form>
+        <Title>Квиз</Title>
+        <Text>Привет! Давай проверим, насколько хорошо ты знаешь столицы</Text>
+        <BtnNext>Далее</BtnNext>
+      </Form>
     </>
   )
 }
