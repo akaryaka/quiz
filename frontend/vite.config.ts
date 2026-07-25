5b8643c20e2a65b08828de4c8669c7961485a5fd
Andrey <akariaka@yandex.ru>
2026-07-25 18:53:40 +0600

    add aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/app': path.resolve(__dirname, './src/app'),
+      '@/assets': path.resolve(__dirname, './src/assets'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/features': path.resolve(__dirname, './src/features'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/shared': path.resolve(__dirname, './src/shared'),
+      '@/styles': path.resolve(__dirname, './src/styles'),
+    }
   }
 })
