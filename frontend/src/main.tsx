153991eba0d7414a815d79faa30954d0c36da711
Andrey <akariaka@yandex.ru>
2026-07-25 19:11:04 +0600

    delete index.css, add alias

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/700.css';
-import './index.css'
-import App from './App.tsx'
+import App from '@/app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
