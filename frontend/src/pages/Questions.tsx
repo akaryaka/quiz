c1a3e80f862b3501eb8bb7c6b1df730ae1d6249b
Andrey <akariaka@yandex.ru>
2026-07-25 19:56:33 +0600

    add comment with code

--- frontend/src/pages/Questions.tsx
+++ frontend/src/pages/Questions.tsx
@@ -1,4 +1,40 @@
 function Questions() {
+  
+  // const [res, newRes] = useState(Object);
+
+  // const handleSubmit = async (event: SubmitEvent) => {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.target);
+  //   const data = Object.fromEntries(formData.entries());
+  //   try {
+  //     const response = await fetch('http://localhost:8000/api/submit', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(data),
+  //     })
+  //     const result = await response.json();
+
+  //     if (response.ok) {
+  //       console.log(result);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // // заменить на axios
+  // useEffect(() => {
+  //   fetch('http://localhost:8000/api/questions')
+  //     .then(res => res.json())
+  //     .then(data => {
+
+  //       newData(data)
+  //     })
+  //     .catch(err => console.error(err));
+  // }, []);
+  
   return (
     <>page Questions</>
   )
