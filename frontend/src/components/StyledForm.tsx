2f2c8bbf4812c87ac0c4e0dae68358119763c2c2
Andrey <akariaka@yandex.ru>
2026-07-26 22:54:56 +0600

    created components

---
+++ frontend/src/components/StyledForm.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border: 1px solid #000;
+  padding: 20px;
+  border-radius: 10px;
+`
