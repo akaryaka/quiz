44efe48f596f273c61db7d0318ecae3deec6832f
Andrey <akariaka@yandex.ru>
2026-07-31 02:00:04 +0600

    ref

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,24 +2,16 @@ import styled from "styled-components"
 import { StyledForm } from "../components/StyledForm"
 import { StyledTitle } from "../components/StyledTitle"
 
+const Text = styled.p`
+  margin-bottom: 25px;
+`
+
 function Home() {
-  const Text = styled.p`
-    margin-bottom: 25px;
-  `
-
-  const BtnNext = styled.button`
-    padding: 5px;
-    width: 30%;
-    cursor: pointer;
-    margin: 0 auto;
-  `
-
   return (
     <>
       <StyledForm>
         <StyledTitle>Квиз</StyledTitle>
         <Text>Привет! Давай проверим, насколько хорошо ты знаешь столицы</Text>
-        <BtnNext>Далее</BtnNext>
       </StyledForm>
     </>
   )
