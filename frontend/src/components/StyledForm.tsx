48b4be2292d0a0e1b2fad0ccda2b0743ed982ba2
Andrey <akariaka@yandex.ru>
2026-08-01 01:33:08 +0600

    ref styles

--- frontend/src/components/StyledForm.tsx
+++ frontend/src/components/StyledForm.tsx
@@ -4,7 +4,11 @@ export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   text-align: center;
+  justify-content: center;
+  align-items: center;
   background-color: #fff;
   padding: 20px;
   border-radius: 10px;
+  width: 530px;
+  height: 400px;
 `
