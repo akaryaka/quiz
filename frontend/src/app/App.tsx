c85b4efc0edd74c5340cce9616d399e7fbf38ff0
Andrey <akariaka@yandex.ru>
2026-08-02 18:46:07 +0600

    feat: btnView; ref styles

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,26 +2,30 @@ import { useState } from 'react';
 import { pages } from './pages';
 import LayoutPage from '../components/LayoutPage';
 import Question from '../components/Question';
+import ResultLayout from '../components/ResultLauot';
 
 
 function App() {
   const [index, setIndex] = useState(0);
   const page = pages[index];
+  const [btnView, setBtnView] = useState(true);
   
   function handleClick() {
+    console.log(index, btnView);
+
     if (index < pages.length-1) {
       setIndex(index + 1);
     } else {
-      setIndex(0)
+      setIndex(0);
     }
   }
 
   return (
     <>
       <div className="wrapper flex justify-center items-center w-screen h-screen bg-[linear-gradient(to_right,#faf0cd,#fab397)]">
-        <div className='container p-[30px] w-[530px] h-[400px] d-flex justify-center items-center rounded-[10px] bg-white'>
-          <LayoutPage title={page.title} content={page.component === 'Questions' ? <Question /> : ''} text={page.text} />
-          <button className='border-green-700 w-50 cursor-pointer p-3 hover:text-white hover:bg-sky-700' onClick={handleClick}>{index == (pages.length - 1) ? 'сброс' : (index == 1 ? '' : 'далее')}</button>
+        <div className='container p-[30px] w-[530px] h-[400px] flex flex-col justify-center rounded-[10px] bg-white'>
+          <LayoutPage title={page.title} content={page.component === 'Questions' ? <Question /> : (page.component === 'Result' ? <ResultLayout />: '')} text={page.text} />
+          {btnView ? <button className='border border-[#fab397] w-50 uppercase cursor-pointer rounded-[10px] p-1 mt-0 mb-0 ml-auto mr-auto transition-all hover:text-white hover:bg-[#fab397]' onClick={handleClick}>{index == (pages.length - 1) ? 'сброс' : 'далее'}</button> : null}
         </div>
       </div>
     </>
