4c1ed0d235354314079b91c73adc0d93dbad5d15
akariaka@yandex.ru <akariaka@yandex.ru>
2026-08-09 15:50:22 +0600

    fixed: project working with server and without

--- frontend/src/components/Question.tsx
+++ frontend/src/components/Question.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import answers from '@/assets/answers.json';
+import questions from '@/assets/questions.json';
 import QuestionsLayout from "./QuestionsLayout";
 
 type Props = {
@@ -13,13 +15,15 @@ type Question = {
 }
 
 function Question({ onClick, resultCount }: Props) {
-  const [data, newData] = useState([]);
+  const [data, newData] = useState<any>([]);
   const [length, setLength] = useState(0)
   const [btnView, setBtnView] = useState(true);
   const [index, setIndex] = useState(0);
   const [btnResultView, setBtnResultView] = useState(false);
-  const [answerStatic, setAnswerStatic] = useState([]);
+  const [load, setLoad] = useState(true);
+  const [checkServer, setCheckServer] = useState(false);
   const questionArr: Question = data[index];
+
   const formStyles = 'flex flex-col justify-center';
   const btnStyles = 'cursor-pointer text-[23px] text-[#fab397] text-center';
 
@@ -28,8 +32,9 @@ function Question({ onClick, resultCount }: Props) {
 
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData.entries());
-   
-    try {
+
+    if(checkServer) {
+      try {
       // заменить на axios
       const response = await fetch('http://localhost:8000/api/submit', {
         method: 'POST',
@@ -47,33 +52,13 @@ function Question({ onClick, resultCount }: Props) {
         }
       }
     } catch (error) {
-      console.log(error);
-      const responseStatic = await fetch('/questions.json', {
-        // нужен get))
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-      // const resultStatic = await responseStatic.json();
-
-      const responseStaticAnswer = await fetch('/answers.json', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-      const resultResponseStaticAnswer = await responseStaticAnswer.json();
-
-      if (responseStatic.ok) {
-        if (Number(Object.values(data)) == resultResponseStaticAnswer[Number(Object.keys(data)) - 1].value) {
-          resultCount((number: number) => number + 1);
-        }
+      console.log(error, ': ошибка при отправке ответа на сервер. Вероятно, сервер недоступен');
+    }
+    } else {
+      if (Number(Object.values(data)) == Number(answers[index].value)) {
+        resultCount((number: number) => number + 1);
       }
     }
-    console.log(Number(Object.values(data)));
 
     if (index < length - 1) {
       setIndex(index + 1);
@@ -90,36 +75,23 @@ function Question({ onClick, resultCount }: Props) {
       .then(data => {
         newData(data)
         setLength(data.length)
+        setLoad(false)
+        setCheckServer(true)
+        console.log(data, ',', load, ',', checkServer);
       })
-      .catch(err => console.error(err));
-  }, []);
+      .catch(err => {
+        console.log(data, ',', load, ',', checkServer);
+        console.error(err, 'файл с бэка не загружен');
+        setLoad(false)
+        newData(questions);
+        setLength(questions.length)
+      });
+  }, []); 
 
-  useEffect(() => {
-    fetch('/questions.json')
-      .then(res => res.json())
-      .then(data => {
-        newData(data)
-        setLength(data.length)
-      })
-      .catch(err => console.error(err));
-  }, [])  
-
-  useEffect(() => {
-    fetch('/answers.json')
-      .then(res => res.json())
-      .then(answerStatic => {
-        setAnswerStatic(answerStatic)
-      })
-      .catch(err => console.error(err));
-  }, []) 
-
-  console.log(answerStatic);
-  
-
-  return (
-    <>
-      <div className="flex justify-center flex-col">
-        <form onSubmit={handleSubmit} className={formStyles}>
+  function Form () {
+    return(
+      <>
+         <form onSubmit={handleSubmit} className={formStyles}>
           <QuestionsLayout
             options={questionArr?.options}
             questionId={questionArr?.id}
@@ -133,6 +105,14 @@ function Question({ onClick, resultCount }: Props) {
           ? <button className={btnStyles} onClick={onClick}>результат</button>
           : null
         }
+      </>
+    )
+  }
+
+  return (
+    <>
+      <div className="flex justify-center flex-col">
+        {load ? 'загрузка...' : <Form />}
       </div>
     </>
   )
