422d9d5d545a790b52d21daf9b450707cbb08c08
andrew <akariaka@yandex.ru>
2026-08-13 00:29:13 +0600

    database check: working

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,5 +1,6 @@
 import express, { type Request, type Response } from 'express';
 import cors from "cors";
+import { Pool } from "pg";
 import questions from './assets/questions.json' with {type: 'json'};
 import answers from './assets/answers.json' with {type: 'json'};
 
@@ -7,17 +8,37 @@ const app = express();
 const port = 8000;
 const typeAnswers: typeAnswer[] | any = answers;
 
-app.use(cors());
-app.use(express.json());
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'quiz',
+  password: 'admin@123',
+  port: 5432,
+});
 
 interface typeAnswer {
   id: number,
   value: number
 }
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/api/questions', (req: Request, res: Response) => {
   res.json(questions)
 })
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * from questions;');
+    res.json({ success: true, time: result.rows[0] });
+    console.log(req.body);
+    
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Ошибка подключения к БД');
+  }
+});
 
 app.post('/api/submit', (req: Request, res: Response) => {
   res.status(200).json(
