272700ceba66621fed562bf95aac9216f9bb5a93
andrew <akariaka@yandex.ru>
2026-08-12 23:46:34 +0600

    ref: index.js=>index.ts

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,12 +1,43 @@
 import express, { type Request, type Response } from 'express';
+import cors from "cors";
+import questions from './assets/questions.json' with {type: 'json'};
+import answers from './assets/answers.json' with {type: 'json'};
 
 const app = express();
-const port = 3000;
+const port = 8000;
+const typeAnswers: typeAnswer[] | any = answers;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello TypeScript + Express!');
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+interface typeAnswer {
+  id: number,
+  value: number
+}
+
+app.get('/api/questions', (req: Request, res: Response) => {
+  res.json(questions)
+})
+
+app.post('/api/submit', (req: Request, res: Response) => {
+  res.status(200).json(
+    {
+      idAnswer: Number(Object.keys(req.body)),
+      answer: checkAnswer()
+    }
+  );
+
+  function checkAnswer(): boolean {
+    let answerCheck: boolean = false;
+    let questionId: number = Number(Object.keys(req.body))-1;
+  
+    if ((typeAnswers[questionId].value) === Number(Object.values(req.body))) {
+      answerCheck = true;
+    }
+
+    return answerCheck;
+  }
+})
+
+
+app.listen(port);
